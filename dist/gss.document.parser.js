b7d2a6ce8175ca3c9d946148fd6dc8758270e4d7
Yaroslaff Fedin <invizko@gmail.com>
2015-03-27 20:18:36 -0700

    New exported compiled

--- dist/gss.document.parser.js
+++ dist/gss.document.parser.js
@@ -6862,114 +6862,165 @@ module.exports = Console;
 
 
 },{}],20:[function(require,module,exports){
-var Exporter,
-  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+var Exporter;
 
 Exporter = (function() {
+  var getIndex, getSelector;
+
   function Exporter(_at_engine) {
     var _ref;
     this.engine = _at_engine;
-    this.postexport = __bind(this.postexport, this);
-    this.preexport = __bind(this.preexport, this);
-    if (!(this.command = typeof location !== "undefined" && location !== null ? (_ref = location.search.match(/export=([a-z0-9]+)/)) != null ? _ref[1] : void 0 : void 0)) {
+    if (!(this.command = typeof location !== "undefined" && location !== null ? (_ref = location.search.match(/export=([a-z0-9,]+)/)) != null ? _ref[1] : void 0 : void 0)) {
       return;
     }
-    this.preexport();
+    if (this.command.indexOf('x') > -1) {
+      this.sizes = this.command.split(',');
+    }
+    this.engine.once('compile', (function(_this) {
+      return function() {
+        return _this.next();
+      };
+    })(this));
   }
 
-  Exporter.prototype.preexport = function() {
-    var baseline, element, height, pairs, scope, width, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref, _ref1, _ref2, _ref3, _ref4, _ref5;
-    if ((scope = this.engine.scope).nodeType === 9) {
-      scope = this.engine.scope.body;
+  Exporter.prototype.text = '';
+
+  getSelector = function(_context) {
+    var index, localName, node, pathSelector, that;
+    index = void 0;
+    localName = void 0;
+    pathSelector = void 0;
+    that = _context;
+    node = void 0;
+    if (that === 'null') {
+      throw 'not an  dom reference';
     }
-    this.engine.identify(scope);
-    _ref = scope.getElementsByTagName('*');
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      element = _ref[_i];
-      if (element.tagName !== 'SCRIPT' && (element.tagName !== 'STYLE' || ((_ref1 = element.getAttribute('type')) != null ? _ref1.indexOf('gss') : void 0) > -1)) {
-        this.engine.identify(element);
+    index = getIndex(that);
+    while (that.tagName) {
+      if (that.id) {
+        pathSelector = '#' + that.id + (pathSelector ? '>' + pathSelector : '');
+        break;
+      } else {
+        pathSelector = that.localName + ':nth-of-type(' + getIndex(that) + ')' + (pathSelector ? '>' + pathSelector : '');
+        that = that.parentNode;
       }
     }
-    if (window.Sizes) {
-      this.sizes = [];
-      _ref2 = window.Sizes;
-      for (_j = 0, _len1 = _ref2.length; _j < _len1; _j++) {
-        pairs = _ref2[_j];
-        _ref3 = pairs[0];
-        for (_k = 0, _len2 = _ref3.length; _k < _len2; _k++) {
-          width = _ref3[_k];
-          _ref4 = pairs[1];
-          for (_l = 0, _len3 = _ref4.length; _l < _len3; _l++) {
-            height = _ref4[_l];
-            this.sizes.push(width + 'x' + height);
+    return pathSelector;
+  };
+
+  getIndex = function(node) {
+    var i, tagName;
+    i = 1;
+    tagName = node.tagName;
+    while (node.previousSibling) {
+      node = node.previousSibling;
+      if (node.nodeType === 1 && tagName.toLowerCase() === node.tagName.toLowerCase()) {
+        i++;
+      }
+    }
+    return i;
+  };
+
+  Exporter.prototype["export"] = function(element, parent, fontSize, unit) {
+    var child, childFontSize, selector, style, styles, text, _i, _len, _ref;
+    if (element == null) {
+      element = document.body.parentNode;
+    }
+    if (fontSize == null) {
+      fontSize = 100;
+    }
+    if (unit == null) {
+      unit = 'em';
+    }
+    text = "";
+    _ref = element.children;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      child = _ref[_i];
+      if (child.tagName === 'STYLE') {
+        if (child.type.indexOf('gss') === -1) {
+          if (child.getAttribute('scoped') !== null && !element.id) {
+            selector = getSelector(element) + ' ';
+          } else {
+            selector = '';
+          }
+          text += Array.prototype.map.call(child.sheet.cssRules, function(rule) {
+            return selector + rule.cssText + '\n';
+          }).join('\n');
+        }
+      } else {
+        styles = window.getComputedStyle(child, null);
+        childFontSize = parseFloat(styles['font-size']);
+        if (style = child.getAttribute('style')) {
+          style = style.replace(/\d+(?:.?\d*?)px/g, function(m) {
+            if (m === '1px') {
+              return m;
+            } else if (unit === 'em') {
+              return parseFloat(m) / childFontSize + 'em';
+            } else {
+              return parseFloat(m) / 16 + 'rem';
+            }
+          });
+        } else {
+          style = '';
+        }
+        if (fontSize !== childFontSize) {
+          if (unit === 'em') {
+            style += 'font-size: ' + (childFontSize / fontSize) + 'em;';
+          } else {
+            style += 'font-size: ' + childFontSize / 16 + 'rem;';
           }
         }
+        if (style) {
+          if (child.id) {
+            selector = '#' + child.id;
+          } else {
+            selector = getSelector(child);
+          }
+          text += selector + '{' + style + '}\n';
+        }
+        text += this["export"](child, element, childFontSize, unit);
       }
     }
-    if (this.command.indexOf('x') > -1) {
-      _ref5 = this.command.split('x'), width = _ref5[0], height = _ref5[1];
-      baseline = 72;
-      width = parseInt(width) * baseline;
-      height = parseInt(height) * baseline;
-      window.addEventListener('load', (function(_this) {
+    return text;
+  };
+
+  Exporter.prototype.next = function() {
+    var height, size, width, _ref;
+    if (size = this.sizes.pop()) {
+      _ref = size.split('x'), width = _ref[0], height = _ref[1];
+      this.engine.then((function(_this) {
         return function() {
-          localStorage[_this.command] = JSON.stringify(_this["export"]());
-          return _this.postexport();
+          debugger;
+          if (_this.previous) {
+            if (_this.sizes.length) {
+              _this.text += '@media (max-width: ' + width + 'px) and (min-width: ' + (_this.previous + 1) + 'px) {';
+            } else {
+              _this.text += '@media (min-width: ' + (_this.previous + 1) + 'px) {';
+            }
+          } else {
+            _this.text += '@media (max-width: ' + width + 'px) {';
+          }
+          _this.text += _this["export"]();
+          _this.text += '}';
+          _this.previous = parseInt(width);
+          return _this.next();
         };
       })(this));
-      document.body.style.width = width + 'px';
-      this.engine.data.properties['::window[height]'] = function() {
-        return height;
-      };
-      return this.engine.data.properties['::window[width]'] = function() {
-        return width;
-      };
+      return this.resize(parseInt(width), parseInt(height));
     } else {
-      if (this.command === 'true') {
-        localStorage.clear();
-        return this.postexport();
-      }
+      return document.write(this.text.split(/\n/g).join('<br>'));
     }
   };
 
-  Exporter.prototype.postexport = function() {
-    var property, result, size, value, _i, _len, _ref;
-    _ref = this.sizes;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      size = _ref[_i];
-      if (!localStorage[size]) {
-        location.search = location.search.replace(/[&?]export=([a-z0-9])+/, '') + '?export=' + size;
-        return;
-      }
-    }
-    result = {};
-    for (property in localStorage) {
-      value = localStorage[property];
-      if (property.match(/^\d+x\d+$/)) {
-        result[property] = JSON.parse(value);
-      }
-    }
-    return document.write(JSON.stringify(result));
-  };
-
-  Exporter.prototype["export"] = function() {
-    var id, index, path, property, value, values, _ref;
-    values = {};
-    _ref = this.engine.values;
-    for (path in _ref) {
-      value = _ref[path];
-      if ((index = path.indexOf('[')) > -1 && path.indexOf('"') === -1) {
-        property = this.engine.data.camelize(path.substring(index + 1, path.length - 1));
-        id = path.substring(0, index);
-        if (property === 'x' || property === 'y' || document.body.style[property] !== void 0) {
-          if (this.engine.values[id + '[intrinsic-' + property + ']'] == null) {
-            values[path] = Math.ceil(value);
-          }
-        }
-      }
-    }
-    values.stylesheets = this.engine.document.Stylesheet["export"]();
-    return values;
+  Exporter.prototype.resize = function(width, height) {
+    console.log('resize', height, width);
+    this.engine.data.properties['::window[height]'] = function() {
+      return height;
+    };
+    this.engine.data.properties['::window[width]'] = function() {
+      return width;
+    };
+    return this.engine.triggerEvent('resize');
   };
 
   return Exporter;
